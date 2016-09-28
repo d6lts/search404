7aa85c93886653882c921b4186ed98e561415b06
akhilavnair_zyxware <akhilavnair_zyxware@3137689.no-reply.drupal.org>
2016-09-28 12:27:27 +0530

    Issue #2807167 by akhilavnair_zyxware: Searching is happened only for the last keyword for multi keyword search in urls and not properly working with custom path

--- src/Controller/Search404Controller.php
+++ src/Controller/Search404Controller.php
@@ -169,17 +169,31 @@ class Search404Controller extends ControllerBase {
     }
     if (\Drupal::config('search404.settings')->get('search404_do_custom_search')) {
       $custom_search_path = \Drupal::config('search404.settings')->get('search404_custom_search_path');
+
       // Remove query parameters before checking whether the search path
       // exists or the user has access rights.
       $custom_search_path_no_query = preg_replace('/\?.*/', '', $custom_search_path);
       $current_path = \Drupal::service('path.current')->getPath();
 
-      if ($current_path == "/" . $keys) {
+      // If search with OR condition eneabled.
+      $search_keys = '';
+      if (\Drupal::config('search404.settings')->get('search404_use_or')) {
+        $search_keys = str_replace(' OR ', '/', $keys);
+      }
+      else {
+        $search_keys = str_replace(' ', '/', $keys);
+      }
+      if ($current_path == "/" . $keys || ($search_keys != '' && $current_path == "/" . $search_keys)) {
         if (!\Drupal::config('search404.settings')->get('search404_disable_error_message')) {
           drupal_set_message(t('The page you requested does not exist. For your convenience, a search was performed using the query %keys.', array('%keys' => Html::escape($keys))), 'error', FALSE);
         }
-        $custom_search_path = str_replace('@keys', $keys, $custom_search_path);
-        $this->search404Goto($custom_search_path);
+        if ($search_keys != '') {
+          $custom_search_path = str_replace('@keys', $search_keys, $custom_search_path);
+        }
+        else {
+          $custom_search_path = str_replace('@keys', $keys, $custom_search_path);
+        }
+        $this->search404Goto("/" . $custom_search_path);
       }
     }
 
@@ -240,51 +254,81 @@ class Search404Controller extends ControllerBase {
    * that resulted in the 404.
    */
   public function search404GetKeys() {
-    $keys = '';
+    $keys = array();
     // Try to get keywords from the search result (if it was one)
     // that resulted in the 404 if the config is set.
     if (\Drupal::config('search404.settings')->get('search404_use_search_engine')) {
       $keys = $this->search404SearchEngineQuery();
     }
+
     // If keys are not yet populated from a search engine referer
     // use keys from the path that resulted in the 404.
     if (empty($keys)) {
       $path = \Drupal::service('path.current')->getPath();
       $paths = explode('/', $path);
-      $keys = end($paths);;
+      // Removing the custom search path value from the keyword search.
+      if (\Drupal::config('search404.settings')->get('search404_do_custom_search')) {
+        $custom_search_path = \Drupal::config('search404.settings')->get('search404_custom_search_path');
+        $custom_search = explode('/', $custom_search_path);
+        $search_path = array_diff($custom_search, array("@keys"));
+        $keywords = array_diff($paths, $search_path);
+        $keys = array_filter($keywords);
+      }
+      else {
+        $keys = array_filter($paths);
+      }
     }
+
     // Abort query on certain extensions, e.g: gif jpg jpeg png.
     $extensions = explode(' ', \Drupal::config('search404.settings')->get('search404_ignore_query'));
     $extensions = trim(implode('|', $extensions));
-    if (!empty($extensions) && preg_match("/\.($extensions)$/i", $keys)) {
-      return FALSE;
+    if (!empty($extensions)) {
+      foreach ($keys as $key) {
+        if (preg_match("/\.($extensions)$/i", $key)) {
+          return FALSE;
+        }
+      }
     }
 
     $regex_filter = \Drupal::config('search404.settings')->get('search404_regex');
     if (!empty($regex_filter)) {
-      $keys = preg_replace("/" . $regex_filter . "/i", '', $keys);
+      foreach ($keys as $key => $value) {
+        $keys[$key] = preg_replace("/" . $regex_filter . "/i", '', $value);
+      }
     }
+
     // Ignore certain extensions from query.
     $extensions = explode(' ', \Drupal::config('search404.settings')->get('search404_ignore_extensions'));
     $extensions = trim(implode('|', $extensions));
     if (!empty($extensions)) {
-      $keys = preg_replace("/\.($extensions)$/i", '', $keys);
+      foreach ($keys as $key => $value) {
+        $keys[$key] = preg_replace("/\.($extensions)$/i", '', $value);
+      }
     }
-    $keys = array($keys);
+
     // Ignore certain words (use case insensitive search).
     $keys = array_udiff($keys, explode(' ', \Drupal::config('search404.settings')->get('search404_ignore')), 'strcasecmp');
     // Sanitize the keys.
     foreach ($keys as $a => $b) {
       $keys[$a] = Html::escape($b);
     }
+
     if (\Drupal::config('search404.settings')->get('search404_use_or')) {
       $path = \Drupal::service('path.current')->getPath();
       $keys = explode('/', substr($path, 1));
+
+      // Removing the custom search path value from the keyword search.
+      if (\Drupal::config('search404.settings')->get('search404_do_custom_search')) {
+        $custom_search_path = \Drupal::config('search404.settings')->get('search404_custom_search_path');
+        $custom_search = explode('/', $custom_search_path);
+        $path = array_diff($custom_search, array("@keys"));
+        $keys = array_diff($keys, $path);
+      }
+
       $keys = trim(implode(' OR ', $keys));
     }
     else {
       $keys = trim(implode(' ', $keys));
-
     }
     return $keys;
   }
